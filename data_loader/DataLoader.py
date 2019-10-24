97d164a6e7656e18ed4b56c03eea2c6904f9cb04
Nikita Shymberg <nikitashymberg@gmail.com>
2019-10-24 12:44:40 -0700

    Used data loader to load data TODO refactor again

--- data_loader/DataLoader.py
+++ data_loader/DataLoader.py
@@ -5,8 +5,9 @@ TODO: usage or something
 import mido
 import os
 import numpy as np
+from tqdm import tqdm
 
-from constants import NOTES_PER_BEAT, NUM_TIMES, NUM_MEASURES, NUM_NOTES
+from constants import NUM_TIMES, NUM_MEASURES, NUM_NOTES
 
 
 class DataLoader():
@@ -21,7 +22,8 @@ class DataLoader():
         self.note_messages = ["note_on", "note_off"]
         self.path = path
 
-    def midi_to_tensor_time(self, time: int, ppq: int, beats_per_measure: int):
+    def midi_to_tensor_time(self, time: int, ppq: int,
+                            beats_per_measure: int) -> int:
         """
         `time` is the absolute tick that the note is played on
         `ppq` is the pulses per quarter note of the song
@@ -29,7 +31,8 @@ class DataLoader():
         """
         return int((time / ppq * (NUM_TIMES / beats_per_measure)) % NUM_TIMES)
 
-    def relative_to_absolute_times(self, notes: [mido.MetaMessage]):
+    def relative_to_absolute_times(self, notes: [mido.MetaMessage]
+                                   ) -> [mido.MetaMessage]:
         """
         Transforms the relative times in `notes` to aboslute times s.t.
         time[i+1] = time[i+1] + time[i]
@@ -38,71 +41,72 @@ class DataLoader():
             notes[i].time += notes[i-1].time
         return notes
 
-    def load_midis(self):
+    def load_midis(self) -> np.ndarray:
         """
+        Loads the midis from self.path and returns them as an nparray
         TODO: add a progress bar
         TODO: ignore multi track files? Ignore drums (channel 10)? Handle it
         """
         all_songs = []
-        for file in os.listdir(self.path):
-            print("Filename:", file)
-            midifile = mido.MidiFile(os.path.join(self.path, file))
+        for file in tqdm(os.listdir(self.path)):
+            try:
+                midifile = mido.MidiFile(os.path.join(self.path, file))
+                messages = mido.merge_tracks(midifile.tracks)
+            except Exception as e:
+                print("ERROR OPENING", file)
+                print(e)
+                continue
+
             ppq = midifile.ticks_per_beat
-            print("PPQ:", ppq)
-            messages = mido.merge_tracks(midifile.tracks)
             time_signatures = [m for m in midifile.tracks[0]
                                if m.type == "time_signature"]
+            if len(time_signatures) == 0:
+                print("ERROR", file, "Doesn't have a time signature")
+                continue
+
             time_signatures = self.relative_to_absolute_times(time_signatures)
 
             song = np.zeros((NUM_MEASURES, NUM_TIMES, NUM_NOTES))
             time = 0
             cur_time_signature = time_signatures.pop(0)
             cur_measure = 0
-            cur_beat = 0
             time_of_prev_measure = 0
             for note in messages:
-                if note.type == "note_on":
-                    assert note.velocity != 0  # Handle this if it happens
-
                 time += note.time
                 # Update time signature
                 if len(time_signatures) and time > time_signatures[0].time:
                     cur_time_signature = time_signatures.pop(0)
-                    print("NEW TIME SIGNATURE:", cur_time_signature.numerator)
 
                 measure_duration = cur_time_signature.numerator * ppq
                 if time >= time_of_prev_measure + measure_duration:
+                    # Update current measure
                     time_since_prev = time - time_of_prev_measure
-                    cur_measure = (cur_measure + time_since_prev // measure_duration) % NUM_MEASURES
+                    cur_measure += time_since_prev // measure_duration
+                    if cur_measure >= NUM_MEASURES:
+                        # Update current song
+                        cur_measure %= NUM_MEASURES
+                        all_songs.append(song)
+                        song = np.zeros((NUM_MEASURES, NUM_TIMES, NUM_NOTES))
                     time_of_prev_measure = time
-                # TODO: do I need the beat??
-                cur_beat = (time % (measure_duration)) // ppq
-                # song_duration = measure_duration * NUM_MEASURES
-                # cur_measure = time % song_duration // (measure_duration)
 
-                # Update song
-                if note.type == "note_on":
+                # Update note
+                if note.type == "note_on" and note.velocity != 0:
                     tensor_time = self.midi_to_tensor_time(
                         time, ppq, cur_time_signature.numerator)
-                    print("Time:", time)
-                    print("Note type:", note.type)
-                    print("Note time:", note.time)
-                    print("Note pitch:", note.note)
-                    print("Beat:", cur_beat)
-                    print("Measure:", cur_measure)
-                    print("Tensor time:", tensor_time)
+                    while note.note >= 96:
+                        note.note -= 12
                     song[cur_measure, tensor_time, note.note] = 1
-                    print("-"*88)
 
-            # print("Number of songs", len(all_music))
-            print("Final time", time)
+            # Save every song into a separate file to save memory
+            np.save("./data/np/" + file, np.asarray(all_songs))
+            all_songs = []
 
-            # print("Time sigs", [msg for msg in time_signatures])
-            break
-        print("-"*88)
+        print("Number of songs", len(all_songs))
+        return np.asarray(all_songs)
         # Note: ticks per note (ppq) could be 960
 
 
 if __name__ == "__main__":
-    data_loader = DataLoader("data")
-    data_loader.load_midis()
+    data_loader = DataLoader("data/midi")
+    data = data_loader.load_midis()
+    # np.save("./midis", data)
