8d7a2d8d7d4852cb24bb06bcc81cbc31fa32cf83
Nikita Shymberg <nikitashymberg@gmail.com>
2019-10-28 14:27:05 -0700

    Added tests for numpytomidi

--- tests/test_MidiNumpyConversion/test_NumpyToMidi.py
+++ tests/test_MidiNumpyConversion/test_NumpyToMidi.py
@@ -3,9 +3,10 @@ This file tests the NumpyToMidi class
 """
 import unittest
 import mido
+import numpy as np
 
 from MidiNumpyConversion.NumpyToMidi import NumpyToMidi
-from constants import NUM_TIMES
+from constants import NUM_TIMES, NUM_NOTES, PPQ
 
 
 class TestNumpyToMidi(unittest.TestCase):
@@ -14,11 +15,26 @@ class TestNumpyToMidi(unittest.TestCase):
     """
     def __init__(self, *args):
         super().__init__(*args)
-        ...
+        self.note_on_msg = "note_on"
+        self.note_off_msg = "note_off"
+        self.track = mido.MidiTrack()
+        self.track.append(mido.Message(self.note_on_msg, note=0))
+        self.note_pitch = 69
+        self.midi_note_time = PPQ * 3.5
+        self.np_note_time = int(NUM_TIMES * 7/8)
+        self.first_midi_note_time = 0
+        self.first_np_note_time = 0
+        self.measure = np.zeros((NUM_TIMES, NUM_NOTES))
+        self.measure[self.np_note_time, self.note_pitch] = 1
+        self.first_measure = np.zeros((NUM_TIMES, NUM_NOTES))
+        self.first_measure[self.first_np_note_time, self.note_pitch] = 1
+        self.time_to_measure_end = 4 * PPQ - self.midi_note_time
+        self.midifile = mido.MidiFile()
+        self.midifile.tracks = self.track
 
     def setUp(self):
         """
-        TODO: implement
+        Creates a NumpyToMidi object to be used by other tests.
         """
         self.ntm = NumpyToMidi()
 
@@ -27,49 +43,70 @@ class TestNumpyToMidi(unittest.TestCase):
         Checks that the add_message function adds the note_on
         message correctly.
         """
-        ...
+        track = self.ntm.add_message(self.track, self.note_pitch,
+                                     self.midi_note_time, msg=self.note_on_msg)
+        self.assertEqual(track[-1].note, self.note_pitch)
+        self.assertEqual(track[-1].time, self.midi_note_time)
+        self.assertEqual(track[-1].type, self.note_on_msg)
 
     def test_add_message_off(self):
         """
         Checks that the add_message function adds the note_off
         message correctly.
         """
-        ...
+        track = self.ntm.add_message(self.track, self.note_pitch,
+                                     self.midi_note_time,
+                                     msg=self.note_off_msg)
+        self.assertEqual(track[-1].note, self.note_pitch)
+        self.assertEqual(track[-1].time, self.midi_note_time)
+        self.assertEqual(track[-1].type, self.note_off_msg)
 
     def test_measure_to_messages_adds_note_on(self):
         """
         Checks that the measure_to_messages function adds the note_on
         message correctly.
         """
-        ...
+        track, _, _ = self.ntm.measure_to_messages(self.measure, 0, [])
+        self.assertEqual(track[-1].note, self.note_pitch)
+        self.assertEqual(track[-1].time, self.midi_note_time)
+        self.assertEqual(track[-1].type, self.note_on_msg)
 
     def test_measure_to_messages_adds_note_off(self):
         """
         Checks that the measure_to_messages function adds the note_off
         message correctly.
         """
-        ...
+        track, _, _ = self.ntm.measure_to_messages(self.first_measure, 0, [])
+        self.assertEqual(track[-1].note, self.note_pitch)
+        self.assertEqual(track[-1].time,
+                         self.first_midi_note_time + self.ntm.note_duration)
+        self.assertEqual(track[-1].type, self.note_off_msg)
 
     def test_measure_to_messages_counts_time(self):
         """
         Checks that the measure_to_messages function returns the time
         between the last note in the measure and the end of it correctly.
         """
-        ...
+        _, time, _ = self.ntm.measure_to_messages(self.measure, 0, [])
+        self.assertEqual(time, self.time_to_measure_end)
 
     def test_measure_to_messages_counts_active_notes(self):
         """
         Checks that the measure_to_messages function returns the number
         of active notes correctly.
         """
-        ...
+        _, time, _ = self.ntm.measure_to_messages(self.measure, 0, [])
+        self.assertEqual(time, self.time_to_measure_end)
 
     def test_add_meta_messages_prepends_messages(self):
         """
         Checks that the add_meta_messages function prepends the
-        correct meta messages to a midi track correctly.
+        track_name and instrument_name meta messages to a midi track correctly.
         """
-        ...
+        track = self.ntm.add_meta_messages(self.track)
+        self.assertEqual(len(track), 3)
+        self.assertEqual(track[0].type, "track_name")
+        self.assertEqual(track[1].type, "instrument_name")
 
     def test_add_conductor_track(self):
         """
@@ -77,4 +114,8 @@ class TestNumpyToMidi(unittest.TestCase):
         to the beginning of a midofile's tracks that contains a 4/4
         time signature message.
         """
-        ...
+        midifile = self.ntm.add_conductor_track(self.midifile)
+        self.assertEqual(len(midifile.tracks), 2)
+        self.assertEqual(midifile.tracks[0][0].type, "time_signature")
+        self.assertEqual(midifile.tracks[0][0].numerator, 4)
+        self.assertEqual(midifile.tracks[0][0].denominator, 4)
